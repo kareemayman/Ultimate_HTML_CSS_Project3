11bac08d0de6cd5a4de9dd1f19e3e32f9a9b00c2
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-10-17 19:53:03 +0300

    Added Smooth Progress Bar Filling Effect When Scrolling

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const scrollButton = document.querySelector('.scroll-to-top')
 
+const skills = document.querySelectorAll('.skills .statistic div')
+
 window.addEventListener('scroll', e => {
 
     const scrollPos = window.scrollY
@@ -11,12 +13,24 @@ window.addEventListener('scroll', e => {
 
         scrollButton.classList.add('invisible')
     }
+
+    if (scrollPos >= 7400) {
+
+        skills.forEach(skill => {
+
+            const prog = skill.dataset.prog
+
+            const span = skill.querySelector('span')
+
+            span.style.width = prog
+        })
+    }
 })
 
 scrollButton.addEventListener('click', e => {
 
     window.scrollTo({
-        
+
         top: 0,
         behavior: "smooth",
     })
